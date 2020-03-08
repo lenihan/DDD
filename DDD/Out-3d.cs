51b777650fdc77166c5362e8356b59311c856244
David Lenihan <davidlenihan@outlook.com>
2020-03-07 23:54:06 -0800

    WIP: Out-3d

---
+++ DDD/Out-3d.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management.Automation;             //Windows PowerShell namespace
+
+namespace DDD
+{
+    [Cmdlet(VerbsData.Out, "3d")]
+    public class Out3dCommand : Cmdlet
+    {
+    }
+}
