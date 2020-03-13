10ce74274053b02c389df7bcbbcc2e1296189528
David Lenihan <davidlenihan@outlook.com>
2020-03-13 09:05:03 -0700

    install-module works, but local testing aliases via new pwsh still do not

--- DDD/doPublish.ps1
+++ DDD/doPublish.ps1
@@ -1,15 +1,26 @@
 Param([Switch]$PowerShellGallery)
 
-$version = '0.0.3'
-$name = 'David Lenihan'
-$moduleName = 'DDD'
-$description = 'Cross-platform (Windows, Linux, Mac) 3D tools for PowerShell.'
-$reqAssemblies = 'DDD.dll'
-$tags = "'3D', 'ply', 'obj', 'Point', 'Matrix', 'Vector'"
-$scripts = "'functions.ps1', 'aliases.ps1'"
-$cmdlets = "'Out-3d'"
+# How to write a PowerShell module manifest
+# https://docs.microsoft.com/en-us/powershell/scripting/developer/module/how-to-write-a-powershell-module-manifest?view=powershell-7
 
 $root = $PSScriptRoot
+$aliasesFile   = 'aliases.ps1'
+$functionsFile = 'functions.ps1'
+
+$version       = '0.0.3'
+$name          = 'David Lenihan'
+$moduleName    = 'DDD'
+$description   = 'Cross-platform (Windows, Linux, Mac) 3D tools for PowerShell.'
+$reqAssemblies = 'DDD.dll'
+$tags          = "'3D', 'ply', 'obj', 'Point', 'Matrix', 'Vector'"
+$scripts       = "'$functionsFile', '$aliasesFile'"
+# $scripts = "'functions.ps1'"
+$cmdlets       = "'Out-3d'"
+$aliases       = "''"
+$functions     = "''"
+$nestedModules = ""
+# $nestedModules = "'$aliasesFile'"
+
 dotnet build $root\DDD.csproj --configuration Release
 $null = mkdir $root\publish\DDD -Force
 try {
@@ -20,6 +31,8 @@ catch {
     Write-Host "Try 'exit' to free file." -ForegroundColor Yellow
     return
 }
+Copy-Item $root\assets\$functionsFile $root\publish\DDD
+Copy-Item $root\assets\$aliasesFile $root\publish\DDD
 New-ModuleManifest -Path $root\publish\DDD\DDD.psd1
 $manifest = Get-Content $root\publish\DDD\DDD.psd1
 $manifest   -replace "^# RootModule = .*$",         "RootModule = '$moduleName'"                        `
@@ -30,7 +43,10 @@ $manifest   -replace "^# RootModule = .*$",         "RootModule = '$moduleName'"
             -replace "^# Description = .*$",        "Description = '$description'"                      `
             -replace "^# RequiredAssemblies = .*$", "RequiredAssemblies = @('$reqAssemblies')"          `
             -replace "^# ScriptsToProcess = .*$",   "ScriptsToProcess = @($scripts)"                    `
+            -replace "^# NestedModules = .*$",      "NestedModules = @($nestedModules)"                 `
+            -replace "^FunctionsToExport = .*$",    "FunctionsToExport = @($functions)"                 `
             -replace "^CmdletsToExport = .*$",      "CmdletsToExport = @($cmdlets)"                     `
+            -replace "^AliasesToExport = .*$",      "AliasesToExport = @($aliases)"                     `
             -replace "# Tags = .*$",                "Tags = @($tags)"                                   `
     | Set-Content $root\publish\DDD\DDD.psd1
 
@@ -40,5 +56,5 @@ if ($PowerShellGallery) {
 else {
     # Publish locally for testing
     Write-Host "# Creating new pwsh. Exit to release files in use." -ForegroundColor Green
-    pwsh -NoExit -Command "& {Write-Host '# Import-Module -Force $root\publish\DDD' -ForegroundColor Green; Import-Module -Force $root\publish\DDD}"
+    pwsh -NoExit -Command "& {Write-Host '# Import-Module -Force -Verbose $root\publish\DDD' -ForegroundColor Green; Import-Module -Force -Verbose $root\publish\DDD}"
 }
