53ab99e677dd17684336cff8d173d5778020a80f
David Lenihan <davidlenihan@outlook.com>
2020-03-12 22:53:51 -0700

    doPublish - rename to avoid tab completion conflict with publish dir, -PowerShellGallery switch for publishing online, local otherwise, local publish does so in new pwsh so that you can exit to free up files in use, detect file in use during copy and give instructions on how to fix issue

--- DDD/Out-3d.cs
+++ DDD/Out-3d.cs
@@ -5,5 +5,22 @@ namespace DDD
     [Cmdlet(VerbsData.Out, "3d")]
     public class Out3dCommand : Cmdlet
     {
+        // Declare the parameters for the cmdlet.
+        [Parameter(Mandatory = true)]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string name;
+
+        // Override the ProcessRecord method to process
+        // the supplied user name and write out a
+        // greeting to the user by calling the WriteObject
+        // method.
+        protected override void ProcessRecord()
+        {
+            WriteObject("Hello " + name + "!");
+        }
     }
 }
--- DDD/doPublish.ps1
+++ DDD/doPublish.ps1
@@ -1,3 +1,5 @@
+Param([Switch]$PowerShellGallery)
+
 $version = '0.0.3'
 $name = 'David Lenihan'
 $moduleName = 'DDD'
@@ -5,11 +7,19 @@ $description = 'Cross-platform (Windows, Linux, Mac) 3D tools for PowerShell.'
 $reqAssemblies = 'DDD.dll'
 $tags = "'3D', 'ply', 'obj', 'Point', 'Matrix', 'Vector'"
 $scripts = "'functions.ps1', 'aliases.ps1'"
+$cmdlets = "'Out-3d'"
 
 $root = $PSScriptRoot
 dotnet build $root\DDD.csproj --configuration Release
 $null = mkdir $root\publish\DDD -Force
-Copy-Item $root\bin\Release\netcoreapp3.1\DDD.dll $root\publish\DDD
+try {
+    Copy-Item $root\bin\Release\netcoreapp3.1\DDD.dll $root\publish\DDD -ErrorAction Stop
+} 
+catch {
+    Write-Host $_.Exception.Message -ForegroundColor Red
+    Write-Host "Try 'exit' to free file." -ForegroundColor Yellow
+    return
+}
 New-ModuleManifest -Path $root\publish\DDD\DDD.psd1
 $manifest = Get-Content $root\publish\DDD\DDD.psd1
 $manifest   -replace "^# RootModule = .*$",         "RootModule = '$moduleName'"                        `
@@ -19,7 +29,16 @@ $manifest   -replace "^# RootModule = .*$",         "RootModule = '$moduleName'"
             -replace "^Copyright = .*$",            "Copyright = '(c) $name. All rights reserved.'"     `
             -replace "^# Description = .*$",        "Description = '$description'"                      `
             -replace "^# RequiredAssemblies = .*$", "RequiredAssemblies = @('$reqAssemblies')"          `
-            -replace "^# ScriptsToProcess = .*$",   "ScriptsToProcess =  @($scripts)"                   `
-            -replace "# Tags = .*$",                "Tags = @($tags)"                                   | Set-Content $root\publish\DDD\DDD.psd1
+            -replace "^# ScriptsToProcess = .*$",   "ScriptsToProcess = @($scripts)"                    `
+            -replace "^CmdletsToExport = .*$",      "CmdletsToExport = @($cmdlets)"                     `
+            -replace "# Tags = .*$",                "Tags = @($tags)"                                   `
+    | Set-Content $root\publish\DDD\DDD.psd1
 
-Publish-Module -Name $root\publish\DDD -NuGetApiKey oy2ig7ftcymwygzfh7oaeychbiaumxyuld27f2zetouyca
+if ($PowerShellGallery) {
+    Publish-Module -Name $root\publish\DDD -NuGetApiKey oy2ig7ftcymwygzfh7oaeychbiaumxyuld27f2zetouyca
+}
+else {
+    # Publish locally for testing
+    Write-Host "# Creating new pwsh. Exit to release files in use." -ForegroundColor Green
+    pwsh -NoExit -Command "& {Write-Host '# Import-Module -Force $root\publish\DDD' -ForegroundColor Green; Import-Module -Force $root\publish\DDD}"
+}
