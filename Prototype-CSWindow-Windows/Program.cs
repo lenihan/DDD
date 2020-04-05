25e867d9d5488404112992ac15d5035f4d0eb8c8
David Lenihan <22015149+lenihan@users.noreply.github.com>
2020-04-05 14:39:31 -0700

    RegisterClass and CreateWindow now work

--- Prototype-CSWindow-Windows/Program.cs
+++ Prototype-CSWindow-Windows/Program.cs
@@ -6,7 +6,7 @@ namespace Prototype_CSWindow_Windows
     class Program
     {
         // From C:\Users\David\src\Avalonia\src\Windows\Avalonia.Win32\Interop\UnmanagedMethods.cs
-
+        public const int CW_USEDEFAULT = unchecked((int)0x80000000);
 
         [Flags]
         public enum ClassStyles : uint
@@ -25,7 +25,58 @@ namespace Prototype_CSWindow_Windows
             CS_IME = 0x00010000,
             CS_DROPSHADOW = 0x00020000
         }
-
+        [Flags]
+        public enum WindowStyles : uint
+        {
+            WS_BORDER = 0x800000,
+            WS_CAPTION = 0xc00000,
+            WS_CHILD = 0x40000000,
+            WS_CLIPCHILDREN = 0x2000000,
+            WS_CLIPSIBLINGS = 0x4000000,
+            WS_DISABLED = 0x8000000,
+            WS_DLGFRAME = 0x400000,
+            WS_GROUP = 0x20000,
+            WS_HSCROLL = 0x100000,
+            WS_MAXIMIZE = 0x1000000,
+            WS_MAXIMIZEBOX = 0x10000,
+            WS_MINIMIZE = 0x20000000,
+            WS_MINIMIZEBOX = 0x20000,
+            WS_OVERLAPPED = 0x0,
+            WS_OVERLAPPEDWINDOW = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_SIZEFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+            WS_POPUP = 0x80000000u,
+            WS_POPUPWINDOW = WS_POPUP | WS_BORDER | WS_SYSMENU,
+            WS_SIZEFRAME = 0x40000,
+            WS_SYSMENU = 0x80000,
+            WS_TABSTOP = 0x10000,
+            WS_VISIBLE = 0x10000000,
+            WS_VSCROLL = 0x200000,
+            WS_EX_DLGMODALFRAME = 0x00000001,
+            WS_EX_NOPARENTNOTIFY = 0x00000004,
+            WS_EX_TOPMOST = 0x00000008,
+            WS_EX_ACCEPTFILES = 0x00000010,
+            WS_EX_TRANSPARENT = 0x00000020,
+            WS_EX_MDICHILD = 0x00000040,
+            WS_EX_TOOLWINDOW = 0x00000080,
+            WS_EX_WINDOWEDGE = 0x00000100,
+            WS_EX_CLIENTEDGE = 0x00000200,
+            WS_EX_CONTEXTHELP = 0x00000400,
+            WS_EX_RIGHT = 0x00001000,
+            WS_EX_LEFT = 0x00000000,
+            WS_EX_RTLREADING = 0x00002000,
+            WS_EX_LTRREADING = 0x00000000,
+            WS_EX_LEFTSCROLLBAR = 0x00004000,
+            WS_EX_RIGHTSCROLLBAR = 0x00000000,
+            WS_EX_CONTROLPARENT = 0x00010000,
+            WS_EX_STATICEDGE = 0x00020000,
+            WS_EX_APPWINDOW = 0x00040000,
+            WS_EX_OVERLAPPEDWINDOW = WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE,
+            WS_EX_PALETTEWINDOW = WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST,
+            WS_EX_LAYERED = 0x00080000,
+            WS_EX_NOINHERITLAYOUT = 0x00100000,
+            WS_EX_LAYOUTRTL = 0x00400000,
+            WS_EX_COMPOSITED = 0x02000000,
+            WS_EX_NOACTIVATE = 0x08000000
+        }
         public delegate IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
 
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
@@ -44,6 +95,9 @@ namespace Prototype_CSWindow_Windows
             public string lpszClassName;
             public IntPtr hIconSm;
         }
+
+        [DllImport("kernel32.dll")]
+        public static extern int GetLastError();
 
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
         public static extern IntPtr GetModuleHandle(string lpModuleName);
@@ -66,30 +120,51 @@ namespace Prototype_CSWindow_Windows
             IntPtr hInstance,
             IntPtr lpParam);
 
-        static IntPtr MyWndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        [DllImport("user32.dll", EntryPoint = "DefWindowProcW")]
+        public static extern IntPtr DefWindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+
+        private static IntPtr MyWndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
         {
-            return IntPtr.Zero;
+            return DefWindowProc(hWnd, msg, wParam, lParam);
         }
         static void Main(string[] args)
         {
-            IntPtr hWnd = IntPtr.Zero;
-            IntPtr hInstance = GetModuleHandle(null);
-            
-            WNDCLASSEX wc = new WNDCLASSEX();
-            wc.style = (int)ClassStyles.CS_OWNDC;
-            wc.lpfnWndProc = MyWndProc;
-            wc.cbClsExtra = 0;
-            wc.cbWndExtra = 0;
-            wc.hInstance = hInstance;
-            wc.hIcon = IntPtr.Zero;
-            wc.hCursor = IntPtr.Zero;
-            wc.hbrBackground = IntPtr.Zero;
-            wc.lpszMenuName = null;
-            wc.lpszClassName = "SimpleWindow";
-
-            if (RegisterClassEx(ref wc) == 0)
+            var wc = new WNDCLASSEX
             {
-                Console.WriteLine("RegisterClassEx failed.");
+                cbSize = Marshal.SizeOf<WNDCLASSEX>(),
+                hInstance = GetModuleHandle(null),
+                lpfnWndProc = new WndProc(MyWndProc),
+                lpszClassName = "SimpleWindow",
+            };
+
+            ushort atom = RegisterClassEx(ref wc);
+            if (atom == 0)
+            {
+                // System Error Codes: https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes
+
+                int error = GetLastError();
+                if (error == 87) Console.WriteLine("RegisterClassEx failed. GetLastError = {0}: The parameter is incorrect.", error);
+                else Console.WriteLine("RegisterClassEx failed. GetLastError = {0}.", error);
+                return;
+            }
+            IntPtr hWnd = CreateWindowEx(
+                0,
+                atom,
+                null,
+                (int)WindowStyles.WS_OVERLAPPEDWINDOW,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                IntPtr.Zero);
+            if (hWnd == IntPtr.Zero)
+            {
+                int error = GetLastError();
+                Console.WriteLine("CreateWindowEx failed. GetLastError = {0}.", error);
                 return;
             }
         }
