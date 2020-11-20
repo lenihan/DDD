afca32a63016d843abc6a615aa0dbbbd8f321b08
David Lenihan <22015149+lenihan@users.noreply.github.com>
2020-11-20 12:08:17 -0800

    simpler sphere demo

--- DDD/demos/sphere.ps1
+++ DDD/demos/sphere.ps1
@@ -3,10 +3,10 @@ Install-Module DDD -Repository PSGallery
 Import-Module DDD
 $points = for($degY = 0; $degY -lt 360; $degY += 10) {
     for($degZ = 0; $degZ -lt 360; $degZ += 10) {
-        $point = New-Point 1 0 0
+        $p = New-Point 1 0 0
         $rotZ = New-RotateZMatrix $degZ
         $rotY = New-RotateYMatrix $degY
-        $rotZ * $rotY * $point
+        $rotZ * $rotY * $p
     } 
 } 
 $points | Out-3d
